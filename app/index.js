dbe1ab61e9928f557ffbd15e61214c28f1953581
Imraan <imraanbernksen.com>
2023-02-15 14:21:48 +0000

    earbuds

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.set("port", process.env.PORT || 1030);
+
+app.use(express.json());
+
+app.use(cors());
+ 
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to Imraan application." });
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`Listening for calls on port ${app.get("port")}`);
+    console.log("Press Ctrl+C to exit server");
+});
